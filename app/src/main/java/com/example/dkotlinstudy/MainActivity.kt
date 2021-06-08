84f24ff264258663251662bdc0741e19155c337f
DESKTOP-OI19NCO\ppyum <doeon93@naver.com>
2021-06-09 01:39:56 +0900

    add : Room 데이터 베이스 예제 추가

--- app/src/main/java/com/example/dkotlinstudy/MainActivity.kt
+++ app/src/main/java/com/example/dkotlinstudy/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.dkotlinstudy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dkotlinstudy.RoomUtil.RoomDataBase
+import com.example.dkotlinstudy.RoomUtil.UserDao
+import com.example.dkotlinstudy.RoomUtil.UserEntity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mUserDao: UserDao
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        accessDatabase()
+        Thread(insertMemo).start()
+    }
+
+    private fun accessDatabase() {
+        val userDatabase = RoomDataBase.getInstance(this)!!
+        mUserDao = userDatabase.userDao()
+    }
+
+    private val insertMemo = Runnable {
+        var newUser = UserEntity("김똥깨", "20", "010-1111-5555")
+        mUserDao.insert(newUser)
     }
 }
